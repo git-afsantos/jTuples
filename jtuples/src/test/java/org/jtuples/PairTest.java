ee8d0e2e88e34e358f41fcfdb7fc1f8bc4c5d63c
git-afsantos <contact.andre.santos@gmail.com>
2015-05-13 21:06:59 +0100

    Added test for Pair#apply.

--- jtuples/src/test/java/org/jtuples/PairTest.java
+++ jtuples/src/test/java/org/jtuples/PairTest.java
@@ -124,5 +124,14 @@ public class PairTest {
 
         assertEquals(pair.hashCode(), other.hashCode());
     }
-    
+
+    @Test
+    public void testApply() {
+        Pair<String, String> pair = new Pair<>("hello", "world");
+
+        assertTrue(pair.apply((a, b) -> {
+            return "hello".equals(a) && "world".equals(b) ?
+                    Boolean.TRUE : Boolean.FALSE;
+        }));
+    }
 }
