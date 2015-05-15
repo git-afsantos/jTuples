4d5a0efa766d6d4a24b4136d99ebdc856dcf26e4
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-05-15 20:00:27 +0100

    Adds tests for IntoPair class.

---
+++ jtuples/src/test/java/org/jtuples/operations/IntoPairTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jtuples.operations;
+
+import org.jtuples.Pair;
+import org.jtuples.Quadruple;
+import org.jtuples.Triple;
+import org.jtuples.Tuple;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Benjamim Sonntag
+ */
+public class IntoPairTest {
+    private IntoPair<String, String> extender;
+    private Tuple expected;
+    private Tuple result;
+    
+    @Before
+    public void setup() {
+        extender = IntoPair.into(new Pair<>("a", "b"));
+    }
+    
+    @Test
+    public void testPutValue() {
+        expected = new Triple<>("a", "b", "c");
+        
+        result = extender.put("c");
+        
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testPutPair() {
+        expected = new Quadruple<>("a", "b", "c", "d");
+        
+        result = extender.put(new Pair<>("c", "d"));
+        
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testPutTriple() {
+        // TODO
+    }
+
+    @Test
+    public void testPutQuadruple() {
+        // TODO
+    }
+    
+}
