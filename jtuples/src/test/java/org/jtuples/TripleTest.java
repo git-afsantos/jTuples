9153fe76c185babd6f26212d37844042950071f8
git-afsantos <contact.andre.santos@gmail.com>
2015-06-27 19:40:55 +0100

    Added new tests to cover the new shift methods.

--- jtuples/src/test/java/org/jtuples/TripleTest.java
+++ jtuples/src/test/java/org/jtuples/TripleTest.java
@@ -138,6 +138,30 @@ public class TripleTest {
     }
 
     @Test
+    public void testShiftLeftWithValue() {
+        expected = new Triple<>("2", "3", "4");
+
+        assertEquals(expected, triple.shiftLeft("4"));
+    }
+
+    @Test
+    public void testShiftLeftWithValueReturnsNew() {
+        assertNotEquals(triple, triple.shiftLeft("4"));
+    }
+
+    @Test
+    public void testShiftLeftWithNull() {
+        expected = new Triple<>("2", "3", null);
+
+        assertEquals(expected, triple.shiftLeft(null));
+    }
+
+    @Test
+    public void testShiftLeftWithNullReturnsNew() {
+        assertNotEquals(triple, triple.shiftLeft(null));
+    }
+
+    @Test
     public void testShiftLeftReturnsNew() {
         assertNotEquals(triple, triple.shiftLeft());
     }
@@ -152,6 +176,30 @@ public class TripleTest {
     @Test
     public void testShiftRightReturnsNew() {
         assertNotEquals(triple, triple.shiftRight());
+    }
+
+    @Test
+    public void testShiftRightWithValue() {
+        expected = new Triple<>("0", "1", "2");
+
+        assertEquals(expected, triple.shiftRight("0"));
+    }
+
+    @Test
+    public void testShiftRightWithValueReturnsNew() {
+        assertNotEquals(triple, triple.shiftRight("0"));
+    }
+
+    @Test
+    public void testShiftRightWithNull() {
+        expected = new Triple<>(null, "1", "2");
+
+        assertEquals(expected, triple.shiftRight(null));
+    }
+
+    @Test
+    public void testShiftRightWithNullReturnsNew() {
+        assertNotEquals(triple, triple.shiftRight(null));
     }
     
     @Test
